456efd4671ad5359710e4ca089fd430486349616
Rueda Matias <36230714+matirue@users.noreply.github.com>
2019-09-09 17:12:29 -0300

    9/9

---
+++ Rueda.Matias/Clase06.Entidades/Tempera.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase06.Entidades
+{
+    public class Tempera
+    {
+        #region Atributos
+
+        private ConsoleColor color;
+        private string marca;
+        private int cantidad;
+
+        #endregion
+
+        #region Constructor
+
+        public Tempera(ConsoleColor color, string marca, int cantidad)
+        {
+            this.color = color;
+            this.marca = marca;
+            this.cantidad = cantidad;
+        }
+
+        #endregion
+
+        #region Metodos
+
+        private string Mostrar()
+        {
+            return "\n Color: " + this.color + " -- Marca: " + this.marca + " -- Cantidad: " + this.cantidad;
+        }
+
+        public static implicit operator string (Tempera obj)
+        {
+            return obj.Mostrar();
+        }
+
+        #endregion
+
+        #region Sobrecarga Operadores
+
+        public static bool operator == (Tempera t1, Tempera t2)
+        {
+            //if ((t1.marca == t2.marca) && (t1.color == t2.color))
+            if ((Object.Equals(t1,null) && Object.Equals(t2,null)) && (t1.marca==t2.marca) && (t1.color == t2.color))            
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator != (Tempera t1, Tempera t2)
+        {
+            return !(t1 == t2);
+        }
+
+        public static Tempera operator +(Tempera t, int cantidad)
+        {
+            t.cantidad += cantidad;
+            return t;
+            
+        }
+        public static Tempera operator +(Tempera t1, Tempera t2)
+        {
+            if (t1.marca==t2.marca && t1.color==t2.color )
+            {
+                t1 += t2.cantidad;
+            }
+            return t1;
+        }
+
+        #endregion
+    }
+}
---
+++ Rueda.Matias/Clase06/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Clase06.Entidades;
+
+namespace Clase06
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tempera obj = new Tempera(ConsoleColor.Black, "marca", 12);
+            //Tempera obj2 = new Tempera(ConsoleColor.Black, "marca", int.MaxValue);
+            //Console.WriteLine(obj == obj2); // true
+            //Console.WriteLine((string)obj); // black -- marca -- 12
+
+
+            //Paleta a = new Paleta(55);
+            Paleta a = 3;
+            Tempera c= new Tempera(ConsoleColor.Red, "aaaa", 2);
+            Tempera d = new Tempera(ConsoleColor.Green, "bbbb", 1);
+
+            //a = 3;
+            a = a + c;
+            a += d;
+            Console.WriteLine(a);
+            Console.ReadKey();
+
+
+        }
+    }
+}
